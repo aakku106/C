d65336d5f7df20f3153e4c05997398f5a4749133
Aakku <adarasha.gaihre106@gmail.com>
2024-03-27 22:00:14 +0545

    more addings

--- exam/strick.c
+++ exam/strick.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+void sup_design();
 void main()
 {
     for (int i = 0; i <= 5; i++)
@@ -24,7 +25,78 @@ void main()
 
     for (int i = 0; i < 5; i++)
     {
-        for(int j = 0; j <= i; j++)
+        int m = 5 - i;
+        for (int space = 0; space < i; space++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < m; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i = 0; i < 5; i++)
+    {
+        int m = 5 - i;
+        for (int space = 0; space < m; space++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    printf("\n");
+    sup_design();
+}
+void sup_design()
+{
+    for (int i = 0; i <= 5; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        int m = 5 - i;
+        for (int space = 0; space < i; space++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < m; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        int m = 5 - i;
+        for (int space = 0; space < m; space++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        int m = 5 - i;
+        for (int j = 0; j < m; j++)
         {
             printf("*");
         }
