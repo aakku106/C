dc70b7f01c07a5e65ed7ab7205b7d32bdfe079fd
Aakku <adarasha.gaihre106@gmail.com>
2024-04-27 16:16:25 +0545

    Refactor printf format in five.c to display employee records in a table format

--- forFinalExamPractise/five.c
+++ forFinalExamPractise/five.c
@@ -14,10 +14,24 @@ int main()
         printf("Enter name, age and address of employee %d: ", i + 1);
         scanf("%s %d %s", emp[i].name, &emp[i].age, emp[i].address);
     }
-    printf("\nName\tAge\tAddress\n");
+    printf("\n | Name\t |Age\t |Address\n");
     for (int i = 0; i < 5; i++)
     {
-        printf("%s\t%d\t%s\n", emp[i].name, emp[i].age, emp[i].address);
+        printf(" | %s \t | %d \t | %s\n", emp[i].name, emp[i].age, emp[i].address);
     }
     return 0;
 }
+/* OUTPUT:
+Enter name, age and address of employee 1: ccn 20 sdhshs
+Enter name, age and address of employee 2: ccn 20 sdhshs
+Enter name, age and address of employee 3: ccn 20 sdhshs
+Enter name, age and address of employee 4: ccn 20 sdhshs
+Enter name, age and address of employee 5: ccn 20 sdhshs
+
+ | Name  |Age    |Address
+ | ccn   | 20    | sdhshs
+ | ccn   | 20    | sdhshs
+ | ccn   | 20    | sdhshs
+ | ccn   | 20    | sdhshs
+ | ccn   | 20    | sdhshs
+*/
