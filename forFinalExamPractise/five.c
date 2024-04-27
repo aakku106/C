652901c7288473aea3927fdef6d3a665f8b080c1
Aakku <adarasha.gaihre106@gmail.com>
2024-04-27 15:54:15 +0545

    Refactor five.c to accept and display records of 5 employees

--- forFinalExamPractise/five.c
+++ forFinalExamPractise/five.c
@@ -1,12 +1,23 @@
 // program to accept record of 5 emploiees having name, age and address and display them.
 #include <stdio.h>
-struct employee
+int main()
 {
-    char name[20];
-    int age;
-    char address[50];
-}
-void main()
-{
-
+    struct employee
+    {
+        char name[30];
+        int age;
+        char address[50];
+    };
+    struct employee emp[5];
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Enter name, age and address of employee %d: ", i + 1);
+        scanf("%s %d %s", emp[i].name, &emp[i].age, emp[i].address);
+    }
+    printf("\nName\tAge\tAddress\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\t%d\t%s\n", emp[i].name, emp[i].age, emp[i].address);
+    }
+    return 0;
 }
